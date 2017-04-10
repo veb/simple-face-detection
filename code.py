a2adc37e72094a0914661c53bffc8ea6af44a267
Mike Mackenzie <mike@veb.co.nz>
2017-04-10 17:37:59 +1200

    update

--- code.py
+++ code.py
@@ -1,38 +1,31 @@
 import web, sys, os, subprocess, uuid
-from opencv.cv import *
-from opencv.highgui import *
+import cv2
 from shutil import copy
 
 def detectObjects(image, imagefile):
-  """Converts an image to grayscale and prints the locations of any
-     faces found"""
-  grayscale = cvCreateImage(cvSize(image.width, image.height), 8, 1)
-  cvCvtColor(image, grayscale, CV_BGR2GRAY)
+    image_read = cv2.imread(imagefile)
+    gray_image = cv2.cvtColor(image_read, cv2.COLOR_BGR2GRAY)
 
-  storage = cvCreateMemStorage(0)
-  cvClearMemStorage(storage)
-  cvEqualizeHist(grayscale, grayscale)
-  cascade = cvLoadHaarClassifierCascade(
-    '/usr/share/doc/opencv-doc/examples/haarcascades/haarcascades/haarcascade_frontalface_default.xml.gz',
-    cvSize(1,1))
-  faces = cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2,
-                             CV_HAAR_DO_CANNY_PRUNING, cvSize(50,50))
+    face_cascade = cv2.CascadeClassifier(
+        '/usr/local/Cellar/opencv3/3.2.0/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
+    )
 
-  if faces.total > 0:
-    for f in faces:
-        print("found a face: [(%d,%d) -> (%d,%d)]" % (f.x, f.y, f.x+f.width, f.y+f.height))
-        foundFace(imagefile, f)
-  return faces.total
+    faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
 
-def foundFace(imagefile, f):
-    #os.system("convert {image} -stroke red -fill red -draw \"rectangle {a},{b} {c},{d}\" {output}".format(
+    for (x, y, w, h) in faces:
+        print("found a face: [(%d,%d) -> (%d,%d)]" % (x, y, x+w, y+h))
+        foundFace(imagefile, (x, y, w, h))
+
+    return faces
+
+def foundFace(imagefile, (x, y, w, h)):
     os.system("convert {image} -draw \"image SrcOver {a},{b} {c},{d} biglazer.png\" {output}".format(
                     image=imagefile,
-                    a=f.x+6,
-                    b=f.y,
-                    c=f.width,
-                    d=f.height,
-                    output=imagefile)); 
+                    a=x+6,
+                    b=y,
+                    c=w,
+                    d=h,
+                    output=imagefile));
 
 urls = (
     '/upload', 'Upload',
@@ -52,26 +45,30 @@ type="image/x-icon"></head><body>
 
     def POST(self):
         x = web.input(myfile={})
-        filedir = '/home/mike/python/uploads' # change this to the directory you want to store the file in.
-        if 'myfile' in x: # to check if the file-object is created
-            filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
-            #filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+        filedir = '/Users/mike/PycharmProjects/thing/uploads'
+        if 'myfile' in x:
+            filepath=x.myfile.filename.replace('\\','/')
+
             filename = str(uuid.uuid4())
-            fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
-            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
-            fout.close() # closes the file, upload complete.
-            # detect face
+            fout = open(filedir +'/'+ filename,'w')
+            fout.write(x.myfile.file.read())
+            fout.close()
+
             imagefile = filedir +'/'+ filename;
-            image = cvLoadImage(imagefile);
+
+            image = cv2.imread(imagefile);
             unique_name = str(uuid.uuid4())
-            output_path = '/home/mike/python/static/%s.png' % unique_name
-            if (detectObjects(image, imagefile) > 0):
+            output_path = '/Users/mike/PycharmProjects/thing/static/%s.png' % unique_name
+            got_objects = detectObjects(image, imagefile)
+            print got_objects[0]
+            if (got_objects.any()):
                 copy(imagefile, output_path)
-                web.redirect("static/%s.png" % unique_name)
+                web.header("Content-Type", "images/png")
+                raise web.seeother("/static/%s.png" % unique_name)
             else:
-                return """No face found."""
+                return
         raise web.seeother('/upload')
 
 if __name__ == "__main__":
-   app = web.application(urls, globals(), True) 
+   app = web.application(urls, globals(), True)
    app.run()
